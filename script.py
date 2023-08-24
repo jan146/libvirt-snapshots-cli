1625b3a005b73ba0cd87bad8eebf7bebaef6e679
jan146 <anurjan@gmail.com>
2023-08-24 19:57:34 +0200

    Basic user menu

--- script.py
+++ script.py
@@ -11,6 +11,26 @@ def checkRoot():
 	if os.geteuid() != 0:
 		print("Warning: non-root user might not have access to the libvirt API", file=sys.stderr)
 
+def inputInt(start: int, end: int) -> int:
+	while True:
+		strInput: str = input("Enter number in range [{:d}-{:d}]: ".format(start, end))
+		try:
+			intInput: int = int(strInput)
+			if start <= intInput <= end:
+				return intInput
+			else:
+				print("Entered number is not in range", file=sys.stderr)
+		except ValueError:
+			print("Input is not an integer", file=sys.stderr)
+
+def menu(options: list[str]) -> int:
+	if len(options) < 2:
+		return 0
+	print()
+	for i, option in enumerate(options):
+		print("\t[{:d}]: \"{:s}\"".format(i + 1, option))
+	return inputInt(start=1, end=len(options))
+
 def main():
 	checkRoot()
 	conn: connType = libvirt.open(libvirtURI)
