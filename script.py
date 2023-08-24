8f2dc7d5a77651fe91da5e2adfbb353d55ec7c9a
jan146 <anurjan@gmail.com>
2023-08-24 22:15:20 +0200

    Let user select action

--- script.py
+++ script.py
@@ -3,13 +3,33 @@ import sys
 import os
 import libvirt
 from typing import TypeAlias
+from enum import Enum
 
 libvirtURI: str = "qemu:///system"
 connType: TypeAlias = libvirt.virConnect
+domainType: TypeAlias = libvirt.virDomain
+
+class Action(Enum):
+	LIST	= 1
+	CREATE	= 2
+	REVERT	= 3
+	DELETE	= 4
+	EXIT	= 5
+
+action2str: dict[Action, str] = {
+	Action.LIST:	"List snapshots",
+	Action.CREATE:	"Create snapshot",
+	Action.REVERT:	"Revert snapshot",
+	Action.DELETE:	"Delete snapshot",
+	Action.EXIT:	"Exit",
+}
 
 def checkRoot():
 	if os.geteuid() != 0:
 		print("Warning: non-root user might not have access to the libvirt API", file=sys.stderr)
+
+def getDomains(conn: connType) -> list[domainType]:
+	return conn.listAllDomains(0)
 
 def inputInt(start: int, end: int) -> int:
 	while True:
@@ -28,12 +48,30 @@ def menu(options: list[str]) -> int:
 		return 0
 	print()
 	for i, option in enumerate(options):
-		print("\t[{:d}]: \"{:s}\"".format(i + 1, option))
+		print("\t[{:d}]: {:s}".format(i + 1, option))
 	return inputInt(start=1, end=len(options))
+
+def menuMain(conn: connType) -> int:
+	return menu(list(action2str.values()))
+
+def executeAction(action: Action):
+	match action:
+		case Action.LIST:
+			pass
+		case Action.CREATE:
+			pass
+		case Action.REVERT:
+			pass
+		case Action.DELETE:
+			pass
+		case Action.EXIT:
+			exit(0)
 
 def main():
 	checkRoot()
 	conn: connType = libvirt.open(libvirtURI)
+	action: Action = Action(menuMain(conn))
+	executeAction(action)
 	conn.close()
 
 main()
